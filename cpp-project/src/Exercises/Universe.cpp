a994a7b737f5e81fb16bd092b65748c9b02a3bd3
Raihanul Islam Nahid <97145938+raihanulislam00@users.noreply.github.com>
2025-03-24 05:18:09 +0600

    Update Universe.cpp

--- cpp-project/src/Exercises/Universe.cpp
+++ cpp-project/src/Exercises/Universe.cpp
@@ -5,7 +5,7 @@
 int Universe::CountAllStars(vector<int>& galaxies) {
     int total_stars = 0;
     for(int stars : galaxies) {
-        total_stars = stars; // fix me!
+        total_stars += stars; // fix me!
     }
     return total_stars;
 }
